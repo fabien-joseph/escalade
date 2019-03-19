7048b58f8182e5997218d05a9951c5c8a07839cd
Ronflonflon <fjoseph24@orange.fr>
2019-03-19 16:55:53 +0100

    Début de création du formulaire de connexion et de la réception des données indiquées

--- escalade-webapp/src/main/java/fr/fabien/escalade/webapp/controller/ConnexionController.java
+++ escalade-webapp/src/main/java/fr/fabien/escalade/webapp/controller/ConnexionController.java
@@ -1,20 +1,23 @@
 package fr.fabien.escalade.webapp.controller;
 
+import fr.fabien.escalade.model.topo.Utilisateur;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequiredArgsConstructor
-@RequestMapping("/connexion")
 public class ConnexionController {
-
-    @GetMapping
-    public String connexion() {
+    @GetMapping ("/connexion")
+    public String connexion(Model model) {
+        model.addAttribute("utilisateur", new Utilisateur());
         return "connexion";
     }
 
+    @PostMapping("/connexion")
+    public String connexionSubmit(@ModelAttribute Utilisateur utilisateur, Model model) {
+        model.addAttribute(utilisateur);
+        return "result";
+    }
 }

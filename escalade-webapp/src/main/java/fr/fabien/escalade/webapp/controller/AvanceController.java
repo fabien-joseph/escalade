84f012712c0580efdaa1ab4a3a7404935a1f214a
Ronflonflon <fjoseph24@orange.fr>
2019-05-06 23:47:48 +0200

    Début de recherche avancée pour les sites

---
+++ escalade-webapp/src/main/java/fr/fabien/escalade/webapp/controller/AvanceController.java
@@ -0,0 +1,51 @@
+package fr.fabien.escalade.webapp.controller;
+
+import fr.fabien.escalade.business.SiteManagement;
+import fr.fabien.escalade.business.UtilisateurManagement;
+import fr.fabien.escalade.model.topo.Site;
+import fr.fabien.escalade.model.topo.Utilisateur;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Controller
+public class AvanceController {
+    @Autowired
+    SiteManagement siteManagement;
+
+    @GetMapping("/test")
+    public String test(Model model, HttpServletRequest request, HttpSession session) {
+        model.addAttribute("site", new Site());
+        return "recherche";
+
+    }
+
+    @PostMapping("/testresult")
+    public String result(@ModelAttribute Site site, Model model){
+        if (site.getNom() == null) {
+            site.setNom("");
+        }
+        if (site.getHauteurMin() == null) {
+            site.setHauteurMin(0);
+        }
+        if (site.getHauteurMax() == null) {
+            site.setHauteurMax(99999);
+        }
+        if (site.getDepartement() == null) {
+            site.setDepartement("");
+        }
+
+        List<Site> sites = siteManagement.findSitesAdvanced(site.getHauteurMin(), site.getHauteurMax(), site.getNom(), site.getDepartement());
+
+        model.addAttribute("sites", sites);
+        return "result";
+    }
+}
